489ca3b38c4319aaf2ba593cfc77400c279aba90
mayaamanda05 <82106729+mayaamanda05@users.noreply.github.com>
2021-05-20 14:23:06 +0700

    Add files via upload

---
+++ DaftarTugas.php
@@ -0,0 +1,20 @@
+<?php ?>
+
+<!doctype html>
+
+<html lang="en">
+
+Tugas 1: <input type="text" name="Tugas 1" value="<?php echo $Tugas 1;?>">
+echo "Today is " . date("Y/m/d") . "<br>";
+
+Tugas 2: <input type="text" name="Tugas 2" value="<?php echo $Tugas 2;?>">
+echo "Today is " . date("Y/m/d") . "<br>";
+
+Tugas 3: <input type="text" name="Tugas 3" value="<?php echo $Tugas 3;?>">
+echo "Today is " . date("Y/m/d") . "<br>";
+
+Tugas 4: <input type="text" name="Tugas 4" value="<?php echo $Tugas 4;?>">
+echo "Today is " . date("Y/m/d") . "<br>";
+
+</body>
+</html>

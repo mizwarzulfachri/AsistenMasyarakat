33bc1892c0f3d52dee715b78dd3dbc2e62236b6d
kautsar abid <82005439+kautsarabidm@users.noreply.github.com>
2021-05-27 03:40:08 +0700

    Update tambah.php

--- Asisten Masyarakat Notifikasi/notif/tambah.php
+++ Asisten Masyarakat Notifikasi/notif/tambah.php
@@ -1,47 +1,64 @@
+<!DOCTYPE html>
 <html>
 <title> Notifikasi Data Tersimpan</title>
 <head>
-<link rel="stylesheet" href="style/bootstrap.min.css" />
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
- 
-<div class="container" style="margin-top:8%">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-           
-            <br>
-           
-                                    <form role="form" method="post" action="input.php">
-                                        <div class="form-group">
-                                            <label>Nama</label>
-                                            <input class="form-control" name="nama">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Jenis Kelamin</label>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="jenis_kelamin" value="Laki-laki" checked>Laki-laki
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="jenis_kelamin" value="Perempuan">Perempuan
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Telepon</label>
-                                            <input class="form-control" name="telepon">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Alamat</label>
-                                            <textarea name="alamat" class="form-control" rows="3"></textarea>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary pull-right">Simpan</button>
-                                        <a href="index.php" class="btn btn-success pull-right" style="margin-right:1%;">Batal</a>
-                                    </form>
-        </div>
+<input type="checkbox" id="check" />
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+      <header>Menu</header>
+      <ul>
+        <li>
+          <a href="#"><i class="fas fa-qrcode"></i>Beranda</a>
+        </li>
+        <li>
+          <a href="#"><i class="fas fa-envelope"></i>Daftar Tugas</a>
+        </li>
+        <li>
+          <a href="#"><i class="fas fa-calendar"></i>Deadline Tugas</a>
+        </li>
+        <li>
+          <a href="#"><i class="fas fa-bell"></i>Pemberitahuan</a>
+        </li>
+      </ul>
     </div>
+    <section>
+
+
+
+    <div class="container2">
+  <form action="/action_page.php">
+    <label for="fname">Nama</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+<br>
+    <label for="jeniskelamin">Male</label>
+    <input type="radio" id="male" name="gender" value="male">
+    <label for="female">Female</label>
+    <input type="radio" id="female" name="gender" value="female">
+<br> <br>
+    <label for="Telepon">Telepon</label>
+    <input type="text" id="telepon" name="telepon" placeholder="your phone number">
+
+    <label for="alamat">Alamat</label>
+    <input type="text" id="alamat" name="alamat">
+    
+  
+    <input type="submit" value="Submit">
+    <a href="index.php" class="btn btn-success pull-right" style="margin-right:1%;">Batal</a>
+  </form>
+</div>
+ 
+
+   
+</div>
+    
+</section>
   
 </div>
 </body>

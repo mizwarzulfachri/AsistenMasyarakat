2be59a1d0300bdafb35a372932ca4354bd55d63a
mizwarzulfachri <76586788+mizwarzulfachri@users.noreply.github.com>
2021-06-04 00:57:31 +0700

    Tambah Login sama Icon

--- Asisten Masyarakat Login/Login.php
+++ Asisten Masyarakat Login/Login.php
@@ -10,14 +10,15 @@
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="../Asisten Masyarakat Login/style.css?v=<?php echo time(); ?>">
-  <link rel="shortcut icon" type="image/png" href="../Asisten Masyarakat/Icon Asisten Masyarakat.png">
+  <link rel="shortcut icon" type="image/png" href="../Asisten Masyarakat icon/Icon Asisten Masyarakat.png">
 
 </head>
 
 <body>
-  <form class="box" action="#" method="post">
-    <img class="img-responsive" src="../Asisten Masyarakat/Logo Asisten Masyarakat.png" alt="" width="350" height="197">
+  <form class="box" action="checker.php" method="post">
+    <img class="img-responsive" src="../Asisten Masyarakat icon/Logo Asisten Masyarakat.png" alt="" width="350" height="197">
 
     <h2>Login</h2>
     <!-- Error -->
@@ -31,6 +32,7 @@
     <input type="password" id="Password" name="Password" placeholder="Kata sandi">
     <input type="submit" name="" value="Masuk">
     
+    <a href="../Asisten Masyarakat Register/Register.php">Belum buat akun?</a>
   </form>
 </body>
 </html>
